77678f6f0134bbf51df38b089f07d7f3d2137fb7
github username <github email address>
2017-09-18 12:52:27 -0400

    Added empty array @@locations to location class and set up #initialize to add location instance to array

--- lib/whats_on_tap/location.rb
+++ lib/whats_on_tap/location.rb
@@ -1,6 +1,13 @@
 class WhatsOnTap::Location
+  @@locations = []
 
   def self.all
-
+    #I should return a bunch of instances of locations
   end
+
+  def initialize
+
+    @@locations << self
+  end
+
 end
