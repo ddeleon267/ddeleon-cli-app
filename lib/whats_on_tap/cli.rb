4950e7e89ece03d3ce419f3515c493079211ca6f
github username <github email address>
2017-09-18 11:44:18 -0400

    Added some initial functionality to #list_locations in cli.rb ; can puts string of locations

--- lib/whats_on_tap/cli.rb
+++ lib/whats_on_tap/cli.rb
@@ -4,5 +4,16 @@ class WhatsOnTap::CLI
 
   def call
     puts "What's on Tap?"
+    list_locations
   end
+
+  def list_locations
+    #ultimately --> would generate list of places nearby. For now:
+    puts <<-DOC
+    1. Hop Angel Brauhaus - 1.0 miles away
+    2. Grey Lodge Pub - 2.7 miles away
+    3. Gat Beer - 2.7 miles away
+    DOC
+  end
+
 end
