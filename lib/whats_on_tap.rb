682e1381f8f9643b17596f73181756366181ada7
github username <github email address>
2017-09-18 11:31:58 -0400

    Successfully set up basic file dependencies with require/require_relative

--- lib/whats_on_tap.rb
+++ lib/whats_on_tap.rb
@@ -1,5 +1,7 @@
-require "whats_on_tap/version"
+require_relative "./whats_on_tap/version"
 
 module WhatsOnTap
   # Your code goes here...
 end
+
+require_relative './whats_on_tap/cli'
--- lib/whats_on_tap/cli.rb
+++ lib/whats_on_tap/cli.rb
@@ -1,3 +1,5 @@
+#This is the CLI controller
+
 class WhatsOnTap::CLI
 
   def call
