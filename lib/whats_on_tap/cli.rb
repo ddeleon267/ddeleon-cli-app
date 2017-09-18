70b449be8b14340a83061864054abca4bf638898
github username <github email address>
2017-09-18 11:58:44 -0400

    Added initial functionality to #list_beers in cli.rb, including case statement

--- lib/whats_on_tap/cli.rb
+++ lib/whats_on_tap/cli.rb
@@ -5,6 +5,7 @@ class WhatsOnTap::CLI
   def call
     puts "What's on Tap?"
     list_locations
+    list_beers
   end
 
   def list_locations
@@ -14,6 +15,19 @@ class WhatsOnTap::CLI
     2. Grey Lodge Pub - 2.7 miles away
     3. Gat Beer - 2.7 miles away
     DOC
+  end
+
+  def list_beers
+    puts "Enter the number of the location for which you'd like to see the beer menu."
+    input = gets.strip
+    case input
+    when "1"
+      puts "#1 beer list"
+    when "2"
+      puts "#2 beer list"
+    when "3"
+      puts "#3 beer list"
+    end
   end
 
 end
