91c2976b771498bb54632885ef5ba04a372f0a03
github username <github email address>
2017-09-19 11:42:46 -0400

    Made some notes to myself in various files as I went through and compared file structure to that of previous labs/projects

--- lib/whats_on_tap.rb
+++ lib/whats_on_tap.rb
@@ -1,4 +1,7 @@
 #maybe this is where you will have to do requiring of scraper stuff (open-ur and nokogiri) plus pry
+#it looks like maybe this is acting as my environment file? I.E. would normally go in its own folder titled 'config' and the file would be called 'environment.rb'
+#I think avi deleted the module below in his video for that reason
+
 
 require_relative "./whats_on_tap/version"
 #maybe
