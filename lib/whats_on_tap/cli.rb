b643e00c0d3c6f563e75deb86cd66986f41b3df1
github username <github email address>
2017-09-21 19:17:05 -0400

    Adjusted the while loops and case statements in cli.rb to avoid getting stuck in a loop at various stages in program; my goal is for the user to be able to exit successfully at any point in the program. However, at some points in the CLI the user will need to enter exit twice before exiting successfully.

--- lib/whats_on_tap/cli.rb
+++ lib/whats_on_tap/cli.rb
@@ -21,54 +21,54 @@ class WhatsOnTap::CLI
   end
 
   def list_beers
-    #asks for prompt and responds to it
     puts ""
     puts "Enter the number of the location for which you'd like to see the beer menu, or type 'list' to see the list of locations again, or type 'exit'."
+    puts ""
     input = nil
     while input != "exit"
       input = gets.strip.downcase
-
       case input
       when "1"
         puts "#1 beer list"
-
+        explore_beer
       when "2"
         puts "#2 beer list"
+        explore_beer
       when "3"
         puts "#3 beer list"
+        explore_beer
       when "list"
         list_locations
       else
-        "Type 'list' or 'exit.'"
+        puts "Type 'list' or 'exit.'"
       end
+
     end
+
   end
 
-  # def explore_beer
-  #   #asks for prompt and responds to it
-  #   puts "Enter the number of the beer you'd like to learn more about, or type 'list' to see the list of locations again, or type 'exit'."
-  #   input_1 = nil
-  #   while input_1 != "exit"
-  #     input_1 = gets.strip.downcase
-  #   #
-  #     case input_1
-  #     when "1"
-  #       puts "some properties about beer #1"
-  #   #     # puts location_1.beer_list
-  #
-  #     when "2"
-  #       puts "some properties about beer #2"
-  #     when "3"
-  #       puts "some properties about beer #3"
-  #     when "list"
-  #       list_locations
-  #     else
-  #       "Type 'list' or 'exit.'"
-  #     end
-  #     goodbye
-  #   end
+  def explore_beer
+    #asks for prompt and responds to it
+    puts "Enter the number of a beer you'd like to learn more about, or type 'list' to see the list of locations again, or type 'exit'."
+    input_1 = nil
+    while input_1 != "exit"
+      input_1 = gets.strip.downcase
+    #
+      case input_1
+      when "1"
+        puts "some properties about beer #1"
+      when "2"
+        puts "some properties about beer #2"
+      when "3"
+        puts "some properties about beer #3"
+      when "list"
+        list_locations
+      else
+        puts "Type 'list' or 'exit.'"
+      end
+    end
 
-#  end
+ end
 
   def goodbye
     puts "Until next time!"
