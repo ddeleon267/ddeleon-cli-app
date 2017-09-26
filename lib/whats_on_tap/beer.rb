de43b1548ed6f8a1284398ba37131e170f98b215
github username <github email address>
2017-09-26 08:01:29 -0400

    In beer.rb changed some of the attr_accessors. Alse added some values for an example beer object to see if I can get the location and beer classes to interact.

--- lib/whats_on_tap/beer.rb
+++ lib/whats_on_tap/beer.rb
@@ -1,12 +1,21 @@
 #might not need to require anything here?
 
 class Beer
-  attr_accessor :name, :type, :notes, :alcohol_percentage, :price
+  attr_accessor :name, :style, :notes, :abv, :price
 
   @beers = []
 
   def self.all
     #I should return a bunch of instances of beers
     self.scrape_beers
+  end
+
+  def self.scrape_beers
+    beer_1 = self.new
+    beer_1.style = "Elderberry Weiss"
+    beer_1.notes = ["2-row pale, wheat malts, Apollo hops, elderberry"]
+    beer_1.abv = "5.5%"
+    beer_1.price = ""
+
   end
 end
