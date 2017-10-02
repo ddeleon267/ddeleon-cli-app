870ee08b7393e7249a6804a6aa535ef63ccfc616
github username <github email address>
2017-10-02 18:46:43 -0400

    Added note in location.rb #scrape_locations and got rid of extra parenthasis

--- lib/whats_on_tap/location.rb
+++ lib/whats_on_tap/location.rb
@@ -10,7 +10,11 @@ class WhatsOnTap::Location
 
     @doc = Nokogiri::HTML(open("https://www.beermenus.com/search?q=philadelphia"))
     places = @doc.css("h3.mb-0.text-normal a").text
+
+    #So a lot of this formatting is particular to the philadelphia list and will need to change when
+    #the gem actually responds to the city input (rather than just defaulting to philly data)
     new_string = places.gsub(/phia/, "phia, ").gsub(/PHIA/, "PHIA, ").split(",").first(10)
+    new_string[8][6]=""
     new_string[9][1]=""
     new_string
 
